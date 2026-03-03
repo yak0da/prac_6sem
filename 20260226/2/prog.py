f669f4adeae69edf43629b0e61014aa5ecc96c98
Airat <101736633+sqdyakoda@users.noreply.github.com>
2026-03-03 17:05:32 +0300

    добавление в обработку команды addmon проверки того, что <name> - имя штатного существа

--- 20260226/2/prog.py
+++ 20260226/2/prog.py
@@ -1,5 +1,5 @@
 import sys
-from cowsay import cowsay
+from cowsay import cowsay, list_cows # в
 
 field = [["" for _ in range(10)] for _ in range(10)]
 curX, curY = 0, 0
@@ -27,13 +27,16 @@ for line in sys.stdin:
     command = line.split()
     if command[0] == "addmon":
         if len(command) == 5 and command[2].isdigit() and command[3].isdigit(): # а
-            name = command[1]         # а
-            hello = command[4]        # а
-            x, y = int(command[2]), int(command[3]) # а
-            print(f"Added monster {name} to ({x}, {y}) saying {hello}")     # а
-            if field[y][x]:
-                print("Replaced the old monster")
-            field[y][x] = (name, hello)         # а
+            if command[1] in list_cows(): # в
+                name = command[1]         # а
+                hello = command[4]        # а
+                x, y = int(command[2]), int(command[3]) # а
+                print(f"Added monster {name} to ({x}, {y}) saying {hello}")     # а
+                if field[y][x]:
+                    print("Replaced the old monster")
+                field[y][x] = (name, hello)         # а
+            else:                                   # в
+                print("Cannot add unknown monster") # в
         else:
             print("Invalid arguments")
     else:
