4ac2446f8c8b09a012846e43de29128111b4fcd1
Airat <101736633+sqdyakoda@users.noreply.github.com>
2026-03-03 14:07:35 +0300

    20260226/1: solved

---
+++ 20260226/1/prog.py
@@ -0,0 +1,40 @@
+import sys
+from cowsay import cowsay
+
+field = [["" for _ in range(10)] for _ in range(10)]
+curX, curY = 0, 0
+
+def encounter(x: int, y: int, text: str):
+    print(cowsay(text))
+
+for line in sys.stdin:
+    line = line.strip()
+    if line in ["up", "down", "left", "right"]:
+        match line:
+            case "up":
+                curY = (curY-1) % 10
+            case "down":
+                curY = (curY+1) % 10
+            case "left":
+                curX = (curX-1) % 10
+            case "right":
+                curX = (curX+1) % 10
+        print(f"Moved to ({curX}, {curY})")
+        if field[curY][curX]:
+            encounter(curX, curY, field[curY][curX])
+        continue
+
+    command = line.split()
+    if command[0] == "addmon":
+        if len(command) == 4 and command[1].isdigit() and command[2].isdigit():
+            x, y = int(command[1]), int(command[2])
+            if field[y][x]:
+                print(f"Added monster to ({x}, {y}) saying {command[3]}")
+                print("Replaced the old monster")
+            else:
+                print(f"Added monster to ({x}, {y}) saying {command[3]}")
+            field[y][x] = command[3]
+        else:
+            print("Invalid arguments")
+    else:
+        print("Invalid command")
