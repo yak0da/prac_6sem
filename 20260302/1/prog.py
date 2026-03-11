09d38f4ac54f67a70dc1d612d3fb104e63b7828c
Airat <101736633+sqdyakoda@users.noreply.github.com>
2026-03-11 09:54:46 +0300

    20260302/1: prog.py supports command addmon with named parameters

--- 20260302/1/prog.py
+++ 20260302/1/prog.py
@@ -1,4 +1,5 @@
 import sys
+import shlex
 from io import StringIO
 from cowsay import cowsay, list_cows, read_dot_cow
 
@@ -16,50 +17,79 @@ $the_cow = <<EOC;
 EOC
 """))
 
-field = [["" for _ in range(10)] for _ in range(10)]
-curX, curY = 0, 0
+class Monster:
+    def __init__(self, name, hello, hitpoints, coords):
+        self.name = name
+        self.hello = hello
+        self.hitpoints = hitpoints
+        self.x, self.y = coords
 
-def encounter(x: int, y: int, obj): # б
-    name, hello = obj               # б
+def encounter(obj):
+    name, hello = obj.name, obj.hello
     if name == "jgsbat":
         print(cowsay(hello, cowfile=jgsbat))
     else:
         print(cowsay(hello, cow=name))
 
-# старт
-print("<<< Welcome to Python-MUD 0.1 >>>")
+def main():
+    field = [[None for _ in range(10)] for _ in range(10)]
+    curX, curY = 0, 0
 
-for line in sys.stdin:
-    line = line.strip()
-    if line in ["up", "down", "left", "right"]:
-        match line:
-            case "up":
-                curY = (curY-1) % 10
-            case "down":
-                curY = (curY+1) % 10
-            case "left":
-                curX = (curX-1) % 10
-            case "right":
-                curX = (curX+1) % 10
-        print(f"Moved to ({curX}, {curY})")
-        if field[curY][curX]:
-            encounter(curX, curY, field[curY][curX])
-        continue
+    # старт
+    print("<<< Welcome to Python-MUD 0.1 >>>")
 
-    command = line.split()
-    if command[0] == "addmon":
-        if len(command) == 5 and command[2].isdigit() and command[3].isdigit(): # а
-            if command[1] in list_cows(): # в
-                name = command[1]         # а
-                hello = command[4]        # а
-                x, y = int(command[2]), int(command[3]) # а
-                print(f"Added monster {name} to ({x}, {y}) saying {hello}")     # а
-                if field[y][x]:
-                    print("Replaced the old monster")
-                field[y][x] = (name, hello)         # а
-            else:                                   # в
-                print("Cannot add unknown monster") # в
+    for line in sys.stdin:
+
+        line = line.strip()
+        if line in ["up", "down", "left", "right"]:
+            match line:
+                case "up":
+                    curY = (curY-1) % 10
+                case "down":
+                    curY = (curY+1) % 10
+                case "left":
+                    curX = (curX-1) % 10
+                case "right":
+                    curX = (curX+1) % 10
+            print(f"Moved to ({curX}, {curY})")
+            if field[curY][curX]:
+                encounter(field[curY][curX])
+            continue
+
+        command = shlex.split(line)
+
+        if not command or command[0] != "addmon":
+            print("Invalid command")
+            continue
         else:
-            print("Invalid arguments")
-    else:
-        print("Invalid command")
+            if len(command) != 9:
+                print("Invalid arguments")
+                continue
+            if not command[1] in [*list_cows(), "jgsbat"]:
+                print("Cannot add unknown monster")
+                continue
+            
+            monster_name = command[1]
+            try:
+                i = command.index("hello")
+                hello_string = command[i + 1]
+                i = command.index("hp")
+                hitpoints = int(command[i + 1])
+                if hitpoints < 0:
+                    raise ValueError("hp должно быть неотрицательным")
+                i = command.index("coords")
+                x, y = int(command[i + 1]), int(command[i + 2])
+                if not (0 <= x < 9 and 0 <= y < 10):
+                    raise ValueError("неверный диапазон для координат")
+            except Exception:
+                print("Invalid arguments")
+                continue
+
+            if field[y][x]:
+                print("Replaced the old monster")
+            field[y][x] = Monster(monster_name, hello_string, hitpoints, (x, y))
+            print(f"Added monster {monster_name} to ({x}, {y}) saying {hello_string}")
+
+
+if __name__ == "__main__":
+    main()
