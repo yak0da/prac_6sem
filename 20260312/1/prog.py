40be14842c41542045d574cde83597046826d755
Airat <101736633+sqdyakoda@users.noreply.github.com>
2026-03-31 19:26:28 +0300

    class MUD_SH added. Reorganization was done

--- 20260312/1/prog.py
+++ 20260312/1/prog.py
@@ -2,6 +2,7 @@ import sys
 import shlex
 from io import StringIO
 from cowsay import cowsay, list_cows, read_dot_cow
+import cmd
 
 jgsbat = read_dot_cow(StringIO(r"""
 $the_cow = <<EOC;
@@ -56,66 +57,42 @@ class Game:
             output.append("Replaced the old monster")
         self.field[x][y] = Monster(name, hello, hp)
         return "\n".join(output)
+    
+class MUD_SH(cmd.Cmd):
+    intro = "<<< Welcome to Python-MUD 0.1 >>>"
+    prompt = "> "
 
-def main():
-    field = [[None for _ in range(10)] for _ in range(10)]
-    curX, curY = 0, 0
+    def __init__(self):
+        super().__init__()
+        self.game = Game()
 
-    # старт
-    print("<<< Welcome to Python-MUD 0.1 >>>")
+    def do_up(self, arg):
+        print(self.game.move(0, -1))
+    def do_down(self, arg):
+        print(self.game.move(0, 1))
+    def do_left(self, arg):
+        print(self.game.move(-1, 0))
+    def do_right(self, arg):
+        print(self.game.move(1, 0))
+    
+    def do_addmon(self, arg):
+        """addmon <monster_name> hello <hello_string> hp <hitpoints> coords <x> <y>"""
+        try:
+            parts = shlex.split(arg)
+            monster_name = parts[0]
+            hello_string = parts[1 + parts.index("hello")]
+            hitpoints = int(parts[1 + parts.index("hp")])
+            i = parts.index("coords")
+            x, y = int(parts[i + 1]), int(parts[i + 2])
 
-    for line in sys.stdin:
+            print(self.game.add_monster(hello_string, hitpoints, monster_name, x, y))
+        except Exception:
+            print("Invalid arguments")
 
-        line = line.strip()
-        if line in ["up", "down", "left", "right"]:
-            match line:
-                case "up":
-                    curY = (curY-1) % 10
-                case "down":
-                    curY = (curY+1) % 10
-                case "left":
-                    curX = (curX-1) % 10
-                case "right":
-                    curX = (curX+1) % 10
-            print(f"Moved to ({curX}, {curY})")
-            if field[curY][curX]:
-                encounter(field[curY][curX])
-            continue
-
-        command = shlex.split(line)
-
-        if not command or command[0] != "addmon":
-            print("Invalid command")
-            continue
-        else:
-            if len(command) != 9:
-                print("Invalid arguments")
-                continue
-            if not command[1] in [*list_cows(), "jgsbat"]:
-                print("Cannot add unknown monster")
-                continue
-            
-            monster_name = command[1]
-            try:
-                i = command.index("hello")
-                hello_string = command[i + 1]
-                i = command.index("hp")
-                hitpoints = int(command[i + 1])
-                if hitpoints < 0:
-                    raise ValueError("hp должно быть неотрицательным")
-                i = command.index("coords")
-                x, y = int(command[i + 1]), int(command[i + 2])
-                if not (0 <= x < 9 and 0 <= y < 10):
-                    raise ValueError("неверный диапазон для координат")
-            except Exception:
-                print("Invalid arguments")
-                continue
-
-            print(f"Added monster {monster_name} to ({x}, {y}) saying {hello_string}")
-            if field[y][x]:
-                print("Replaced the old monster")
-            field[y][x] = Monster(monster_name, hello_string, hitpoints, (x, y))
+    def do_EOF(self, arg):
+        print()
+        return True
 
 
 if __name__ == "__main__":
-    main()
+    MUD_SH().cmdloop()
